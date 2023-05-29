e279d8c4343e85005916bb75e74fefa68c937b9b
Wadrian Araujo <wadm.snf20@uea.edu.br>
2023-05-29 17:39:52 -0400

    added functions for take image on camera and storage

--- app/src/main/java/com/weadrix/birdclassifier/MainActivity.kt
+++ app/src/main/java/com/weadrix/birdclassifier/MainActivity.kt
@@ -1,11 +1,108 @@
 package com.weadrix.birdclassifier
 
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import androidx.core.graphics.createBitmap
+import com.weadrix.birdclassifier.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var imageView: ImageView
+    private lateinit var buttonCapture: Button
+    private lateinit var tvOutput: TextView
+    private val GALLERY_REQUEST_CODE = 123
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        imageView = binding.imageView
+        buttonCapture = binding.btnCaptureImage
+        tvOutput = binding.tvOutput
+        val buttonLoad = binding.btnLoadImage
+
+        buttonCapture.setOnClickListener{
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+            {
+                takePicturePreview.launch(null)
+            }
+            else{
+                requestPermission.launch(android.Manifest.permission.CAMERA)
+            }
+        }
+
+        buttonLoad.setOnClickListener{
+            if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                 val intent = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                intent.type = "image/*"
+                val mimeType = arrayOf("image/jpeg","image/png","image/jpg")
+                intent.putExtra(Intent.EXTRA_MIME_TYPES,mimeType)
+                intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                onResult.launch(intent)
+            }else{
+                requestPermission.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+    }
+
+    //Request Camera permission
+    private val requestPermission = registerForActivityResult(ActivityResultContracts.RequestPermission()){granted->
+        if (granted){
+            takePicturePreview.launch(null)
+        }else{
+            Toast.makeText(this, "Permission Denied!!! Try again",Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    //Active Camera And Take a shot
+    private val takePicturePreview = registerForActivityResult(ActivityResultContracts.TakePicturePreview()){bitmap->
+        if (bitmap!=null){
+            imageView.setImageBitmap(bitmap)
+            outputGenerator(bitmap)
+        }
+    }
+
+    //take image from storage
+    private val onResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
+        Log.i("TAG","This is the result: ${result.data} ${result.resultCode}")
+        onResultReceived(GALLERY_REQUEST_CODE)
+    }
+
+    private fun onResultReceived(requestCode: Int, result: ActivityResult?){
+        when(requestCode){
+            GALLERY_REQUEST_CODE->{
+                if (result?.resultCode == Activity.RESULT_OK){
+                    result.data?.data?.let{uri ->
+                        Log.i("TAG","onResultReceived: $uri")
+                        val bitmap = BitmapFactory.decodeStream(contentResolver.openInputStream(uri))
+                        imageView.setImageBitmap(bitmap)
+                        outputGenerator(bitmap)
+                    }
+                }else{
+                    Log.e("Tag","onActivityResult: error in selecting image")
+                }
+            }
+        }
+    }
+
+    private fun outputGenerator(bitmap: Bitmap){
+
     }
 }
