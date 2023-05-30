8546fb7e43eb8f989f9ae4e57f97e19acc3799f4
Wadrian Araujo <wadm.snf20@uea.edu.br>
2023-05-30 18:31:14 -0400

    add onClick methods, generator image inference, etc...
    added storage permission request
    added request to use the camera
    added the ability to take a photo
    added image download taken
    added inference result with the ability to redirect to google with the name of the bird

--- app/src/main/java/com/weadrix/birdclassifier/MainActivity.kt
+++ app/src/main/java/com/weadrix/birdclassifier/MainActivity.kt
@@ -1,11 +1,15 @@
 package com.weadrix.birdclassifier
 
 import android.app.Activity
+import android.app.AlertDialog
 import android.app.Instrumentation
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -19,12 +23,16 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.graphics.createBitmap
 import com.weadrix.birdclassifier.databinding.ActivityMainBinding
+import com.weadrix.birdclassifier.ml.BirdsModeI
+import org.tensorflow.lite.support.image.TensorImage
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
     private lateinit var imageView: ImageView
     private lateinit var buttonCapture: Button
+    private lateinit var buttonLoad: Button
     private lateinit var tvOutput: TextView
     private val GALLERY_REQUEST_CODE = 123
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,7 +44,7 @@ class MainActivity : AppCompatActivity() {
         imageView = binding.imageView
         buttonCapture = binding.btnCaptureImage
         tvOutput = binding.tvOutput
-        val buttonLoad = binding.btnLoadImage
+        buttonLoad = binding.btnLoadImage
 
         buttonCapture.setOnClickListener{
             if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
@@ -60,6 +68,56 @@ class MainActivity : AppCompatActivity() {
                 requestPermission.launch(android.Manifest.permission.READ_EXTERNAL_STORAGE)
             }
         }
+
+        // Redirect user to google search for the bird
+        tvOutput.setOnClickListener{
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com/search?q=${tvOutput.text}"))
+            startActivity(intent)
+        }
+
+        imageView.setOnLongClickListener{
+            requestPermissionLaunch.launch(android.Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            return@setOnLongClickListener true
+        }
+    }
+
+    private val requestPermissionLaunch = registerForActivityResult(ActivityResultContracts.RequestPermission()){isGranted:Boolean ->
+        if (isGranted){
+            AlertDialog.Builder(this).setTitle("Download Imgae?")
+                .setMessage("Do you want to download this image to yout device?")
+                .setPositiveButton("Yes"){_, _ ->
+                    val drawable:BitmapDrawable = imageView.drawable as BitmapDrawable
+                    val bitmap = drawable.bitmap
+                    downloadImage(bitmap)
+                }
+                .setNegativeButton("No"){dialog, _ ->
+                    dialog.dismiss()
+                }.show()
+        }else{
+            Toast.makeText(this,"Please allow permission to download image", Toast.LENGTH_LONG).show()
+        }
+    }
+
+    // Fun that takes a bitmap and store
+    private fun downloadImage(mBitmap: Bitmap):Uri? {
+        val contentValues = ContentValues().apply {
+            put(MediaStore.Images.Media.DISPLAY_NAME,"Birds_Images" + System.currentTimeMillis()/1000)
+            put(MediaStore.Images.Media.MIME_TYPE,"Image/png")
+        }
+        val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        if (uri != null){
+            contentResolver.insert(uri, contentValues)?.also {
+                contentResolver.openOutputStream(it).use {outputStream ->
+                    if (!mBitmap.compress(Bitmap.CompressFormat.PNG,100,outputStream)){
+                        throw  IOException("Couldn't save the bitmap")
+                    }else{
+                        Toast.makeText(applicationContext,"Image Saved", Toast.LENGTH_LONG).show()
+                    }
+                }
+                return it
+            }
+        }
+        return null
     }
 
     //Request Camera permission
@@ -82,7 +140,7 @@ class MainActivity : AppCompatActivity() {
     //take image from storage
     private val onResult = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
         Log.i("TAG","This is the result: ${result.data} ${result.resultCode}")
-        onResultReceived(GALLERY_REQUEST_CODE)
+        onResultReceived(GALLERY_REQUEST_CODE,result)
     }
 
     private fun onResultReceived(requestCode: Int, result: ActivityResult?){
@@ -102,7 +160,26 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun outputGenerator(bitmap: Bitmap){
 
+
+    private fun outputGenerator(bitmap: Bitmap){
+        // Tensorflow lite model variable
+        val birdModel = BirdsModeI.newInstance(this)
+
+        // Converting bitmap into tensor flow image
+        val newBitmap = bitmap.copy(Bitmap.Config.ARGB_8888,true)
+        val tfImage = TensorImage.fromBitmap(newBitmap)
+
+        // Runs model inference and gets result.
+        val outputs = birdModel.process(tfImage).probabilityAsCategoryList.apply {
+            sortByDescending { it.score }
+        }
+
+        // Result with high probability
+        val highProbabilityOutput = outputs[0]
+
+        // Setting output text
+        tvOutput.text = highProbabilityOutput.label
+        Log.i("TAG","outputGenerator: $highProbabilityOutput")
     }
 }
